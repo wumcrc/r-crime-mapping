2d9c41ca36f7d61709eee1c2c2b870db366c0d5f
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-09-06 11:02:52 -0500

    Create cs_filter_crime_error.R

---
+++ notebook/cs_filter_crime_error.R
@@ -0,0 +1,13 @@
+# Load Dependencies
+library(compstatr)
+
+# Load Index & Data
+i <- cs_create_index()
+june2018 <- cs_get_data(year = 2018, month = "june", index = i)
+
+# subset rapes and removed unfounded incidents
+juneRapes <- cs_filter_count(june2018, var = count)
+juneRapes <- cs_filter_crime(juneRapes, var = crime, crime = "rape")
+
+# View First Part of Object
+head(juneRapes)
