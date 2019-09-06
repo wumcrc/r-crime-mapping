db2da5455780bd2434ffd4adb455bb62e8d1f958
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-09-06 13:01:09 -0500

    Update cs_filter_crime_error.R

--- notebook/cs_filter_crime_error.R
+++ notebook/cs_filter_crime_error.R
@@ -5,9 +5,16 @@ library(compstatr)
 i <- cs_create_index()
 june2018 <- cs_get_data(year = 2018, month = "june", index = i)
 
+# add Categories
+june2018 <- cs_filter_count(june2018, var = count)
+june2018 <- cs_crime_cat(june2018, var = crime, crimeCatNum, "numeric")
+june2018 <- cs_crime_cat(june2018, var = crime, crimeCatName, "string")
+
 # subset rapes and removed unfounded incidents
 juneRapes <- cs_filter_count(june2018, var = count)
 juneRapes <- cs_filter_crime(juneRapes, var = crime, crime = "rape")
 
+
+
 # View First Part of Object
-head(juneRapes)
+juneRapes
