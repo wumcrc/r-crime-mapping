97208ebc543f9c2a165eec817a8ccc5f62affdf0
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-09-09 14:40:09 -0500

    addition of external data

---
+++ notebook/external_pop_data.R
@@ -0,0 +1,13 @@
+# Load Dependencies
+
+library(readxl)
+library(here)
+
+# Read in Neighborhood Population Data
+
+here()
+nbhd_pop10 <- read_xlsx(here("data/nbhd_pop10.xlsx"))
+
+#Save as R Data File
+
+save(nbhd_pop10, file = "data/nbhd_pop10.rda")
