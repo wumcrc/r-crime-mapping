87d0f45b0c4d643d1231067aa41823f7c524e68e
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-09-16 17:12:21 -0500

    updates to code
    updated code includes changing call to a variable, and removing added objects from the environment
    unnamed r chunks were named and a few were re-organized for readability

--- src/R/external_data.R
+++ src/R/external_data.R
@@ -2,12 +2,17 @@
 
 library(readxl)
 library(here)
+library(tidyr)
 
 # Read in Neighborhood Population Data
 
 here()
 nbhd_pop10 <- read_xlsx(here("data/external/nbhd_pop10.xlsx")) %>% 
   rename(neighborhood = nbhdNum)
+
+nbhd_pop10[nbhd_pop10 == 0] <- NA
+
+
 
 #Save as R Data File
 
