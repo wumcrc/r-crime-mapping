f94e2ecae7e745fa07500ba2a1bcdce97be055b8
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-09-16 14:44:18 -0500

    update external_data.R
    added code to rename variable for table join

--- src/R/external_data.R
+++ src/R/external_data.R
@@ -6,8 +6,10 @@ library(here)
 # Read in Neighborhood Population Data
 
 here()
-nbhd_pop10 <- read_xlsx(here("data/external/nbhd_pop10.xlsx"))
+nbhd_pop10 <- read_xlsx(here("data/external/nbhd_pop10.xlsx")) %>% 
+  rename(neighborhood = nbhdNum)
 
 #Save as R Data File
+
 
 save(nbhd_pop10, file = "data/nbhd_pop10.rda")
