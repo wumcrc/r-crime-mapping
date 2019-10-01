7bdffe84690993dd8d07b88022904ef42926fd10
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-10-01 16:16:06 -0500

    Create sdb_total_crimes_2018_2019.R

---
+++ src/R/sdb_total_crimes_2018_2019.R
@@ -0,0 +1,78 @@
+# Load Library 
+library(compstatr)
+library(dplyr)
+library(lubridate)
+library(writexl)
+library(here)
+
+# Create Index
+i <- cs_create_index()
+
+# Get data 
+yearlist18 <- cs_get_data(year = 2018, index = i)
+yearlist19 <- cs_get_data(year = 2019, index = i)
+
+# Validate Data 
+cs_validate(yearlist18, year = 2018)
+cs_validate(yearlist19, year = 2019)
+
+# Collapse Data 
+reports18 <- cs_collapse(yearlist18)
+reports19 <- cs_collapse(yearlist19)
+
+# Total Crimes 
+crimes18 <- cs_combine(type = "year", date = 2018, reports18, reports19)
+crimes19 <- cs_combine(type = "year", date = 2019, reports18, reports19)
+
+# Join Crimes 
+crimes <- full_join(crimes18, crimes19)
+
+# Extract Skinker DeBaliviere 
+sdb_crimes <- filter(crimes, neighborhood == 46)
+
+# Add Crime Details 
+sdb_crimes_tidy <- sdb_crimes %>% 
+  cs_filter_count(., var = count) %>%                                           # Removes Negative Counts
+  cs_filter_crime(., var = crime, "part 1") %>%                                 # Filters Part 1 Crimes 
+  cs_crime_cat(., var = crime, crimeCatNum, "numeric") %>%                      
+  cs_crime_cat(., var = crime, crimeCatName, "string") %>%                      # Labels Crime Categories
+  cs_crime(., var = crime, violent, "violent") %>%                              # Labels Violent Crimes 
+  cs_crime(., var = crime, property, "property") %>%                            # Labels Property Crimes
+  cs_crime(., var = crime, larceny, 6) %>%                                      # Labels Larceny Crimes
+  cs_parse_date(., date_occur, dateVar = dateOcc, timeVar = timeOcc) %>%        # Parses Date Data
+  mutate(weekday = wday(dateOcc, label = TRUE)) %>%                             # Labels Days of the Week
+  mutate(monthVar = dateOcc) %>%
+  mutate(tod = timeOcc)
+
+sdb_crimes_tidy$monthVar <- month(as.Date(sdb_crimes_tidy$monthVar, 
+                                      format="%d/%m/%Y"), label = TRUE)
+
+sdb_crimes_tidy$tod <- strptime(sdb_crimes_tidy$tod, 
+                            tz = "America/Chicago", "%H:%M")
+
+sdb_crimes_tidy$tod <- format(sdb_crimes_tidy$tod, 
+                          format = "%H%M%S")
+
+sdb_crimes_tidy <- sdb_crimes_tidy %>%
+  mutate(., dayNight = ifelse(tod >= "180000" & tod < "600000", "Night", "Day")) %>% 
+  dplyr::select(-dateTime, -tod, -flag_crime, 
+                -flag_administrative, -flag_unfounded, -flag_cleanup)  %>%
+  cs_missingXY(., varX = x_coord, varY = y_coord, newVar = missing) %>% 
+  cs_replace0(., var = x_coord) %>%
+  cs_replace0(., var = y_coord) 
+
+sdb_crimes_tidy <- mutate(sdb_crimes_tidy, "type" = ifelse(grepl("ALL OTHER", description), "All Other", 
+                                           ifelse(grepl("BICYCLE", description), "Bicycle", 
+                                                  ifelse(grepl("FROM BUILDING", description), "From Building", 
+                                                         ifelse(grepl("FROM COIN", description), "From Coin Machine", 
+                                                                ifelse(grepl("FROM MTR VEH", description), "From Motor Vehicle", 
+                                                                       ifelse(grepl("MTR VEH PARTS", description), "Motor Vehicle Parts",
+                                                                              ifelse(grepl("PURSE", description), "Purse Snatching",
+                                                                                     ifelse(grepl("SHOPLIFT", description), "Shoplifting",
+                                                                                            ifelse(grepl("FRM PRSN", description), "From Person", NA))))))))))
+
+sdb_crimes_tidy <- mutate(sdb_crimes_tidy, "value" = ifelse(grepl("UNDER", description), "Under $500",
+                                            ifelse(grepl("\\$500 - \\$24,999", description), "$500 - $24,999",
+                                                   ifelse(grepl("OVER \\$25,000", description), "Over $25,000", NA))))
+
+write_xlsx(sdb_crimes_tidy, here("results", "sdb_total_crimes_2018_2019.xlsx"))
